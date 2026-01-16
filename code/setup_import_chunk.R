b5f6e821663528873f7b5d3da2eaf3bf505f2bb7
swbioinf <s.williams@qcif.edu.au>
2026-01-16 03:49:16 +0000

    save noninteractive chunk

--- code/setup_import_chunk.R
+++ code/setup_import_chunk.R
@@ -1,10 +1,10 @@
 
 # for building htmls to share, take out of final version
 
-```{r results='hide', echo=FALSE}
-#Ignore this chunk, it is only to run the setup Rmarkdown when knitting 
-building_htmls <- ! interactive()
-if (building_htmls ) {
-  source(knitr::purl("00_Setup.Rmd", quiet=TRUE))
-}
+```{r echo=FALSE, results='hide', message=FALSE, warning=FALSE}
+# This code will run the setup chunk if this is built automatically, f
+# it is for building reference htmls.
+# When running interactively (via Rstudio) it will not run (not needed)
+# This chunk may be ignored during workshops or when running through material.
+if (! base::interactive()) { source("00_Setup.R") }
 ```
