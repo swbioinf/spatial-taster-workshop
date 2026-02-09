16d168617cc58ad206b13d35575050d9b2f16e7c
swbioinf <s.williams@qcif.edu.au>
2026-02-09 07:03:30 +0000

    rendering script now adds HOME

--- code/render_htmls.R
+++ code/render_htmls.R
@@ -1,9 +1,17 @@
 print("Rendering workshop files...")
+
+# Pandoc needs HOME set. 
+# This doesn't seem to be set when running as workbench job. (But it is as a 'background' job)
+# So set upt explicitly to my home directory here (not ~).
+Sys.setenv(HOME="/home/s.williams")
+
+
 rmarkdown::render("01_Explore.Rmd",              output_dir="docs/") #catch failures earlier
 rmarkdown::render("index.Rmd",                   output_dir="docs/")
 rmarkdown::render("00_Setup.Rmd",                output_dir="docs/")
-rmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="docs/")
-rmarkdown::render("03_DE_by_niche.Rmd",          output_dir="docs/")
+rrmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="docs/")
+rrmarkdown::render("03_DE_by_niche.Rmd",          output_dir="docs/")
 rmarkdown::render("04_DE_by_localisation.Rmd",   output_dir="docs/")
 rmarkdown::render("05_celltype_cooccurance.Rmd", output_dir="docs/")
+
 print("Done.")
