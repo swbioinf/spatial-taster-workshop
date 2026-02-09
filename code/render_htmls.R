d65095af90b0232ee47fdcc5f73b9be92b117296
swbioinf <s.williams@qcif.edu.au>
2026-02-09 06:18:06 +0000

    adding rendering script

--- code/render_htmls.R
+++ code/render_htmls.R
@@ -1,9 +1,9 @@
 print("Rendering workshop files...")
-rmarkdown::render("01_Explore.Rmd",              output_dir="public/") #catch failures earlier
-rmarkdown::render("index.Rmd",                   output_dir="public/")
-rmarkdown::render("00_Setup.Rmd",                output_dir="public/")
-rmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="public/")
-rmarkdown::render("03_DE_by_niche.Rmd",          output_dir="public/")
-rmarkdown::render("04_DE_by_localisation.Rmd",   output_dir="public/")
-rmarkdown::render("05_celltype_cooccurance.Rmd", output_dir="public/")
+rmarkdown::render("01_Explore.Rmd",              output_dir="docs/") #catch failures earlier
+rmarkdown::render("index.Rmd",                   output_dir="docs/")
+rmarkdown::render("00_Setup.Rmd",                output_dir="docs/")
+rmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="docs/")
+rmarkdown::render("03_DE_by_niche.Rmd",          output_dir="docs/")
+rmarkdown::render("04_DE_by_localisation.Rmd",   output_dir="docs/")
+rmarkdown::render("05_celltype_cooccurance.Rmd", output_dir="docs/")
 print("Done.")
