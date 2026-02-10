e15ff5d196df5f78aec5eedd327a52de7edfe4d3
swbioinf <s.williams@qcif.edu.au>
2026-02-10 06:23:37 +0000

    render htmls fix, adding links back to 02, and doc rebuild

--- code/render_htmls.R
+++ code/render_htmls.R
@@ -9,8 +9,8 @@ Sys.setenv(HOME="/home/s.williams")
 rmarkdown::render("01_Explore.Rmd",              output_dir="docs/") #catch failures earlier
 rmarkdown::render("index.Rmd",                   output_dir="docs/")
 rmarkdown::render("00_Setup.Rmd",                output_dir="docs/")
-rrmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="docs/")
-rrmarkdown::render("03_DE_by_niche.Rmd",          output_dir="docs/")
+rmarkdown::render("02_DE_by_celltype.Rmd",       output_dir="docs/")
+rmarkdown::render("03_DE_by_niche.Rmd",          output_dir="docs/")
 rmarkdown::render("04_DE_by_localisation.Rmd",   output_dir="docs/")
 rmarkdown::render("05_celltype_cooccurance.Rmd", output_dir="docs/")
 
